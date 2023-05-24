b77c7a912e529f8fa65ac9a879ee5d12c44b2860
Mehslos <134284173+Mehslos@users.noreply.github.com>
2023-05-24 06:35:22 +0530

    Update bn.py

--- bn.py
+++ bn.py
@@ -1,4 +1,8 @@
-# ------------------------------------------------------------ #
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.utils import ChromeType
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException, WebDriverException
 from selenium.webdriver.chrome.options import Options
@@ -8,44 +12,34 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+import tkinter as tk
 import names
 import secrets
 import random
 import string
 import os
-# ------------------------------------------------------------
-CHROMEDRIVER_PATH = os.environ.get('CHROMEDRIVER_PATH', '/usr/local/bin/chromedriver')
-GOOGLE_CHROME_BIN = os.environ.get('GOOGLE_CHROME_BIN', '/usr/bin/google-chrome')
-options = Options()
-options.binary_location = GOOGLE_CHROME_BIN
-options.add_argument('--disable-gpu')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
-options.headless = True
-driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH , chrome_options=options)
-# ------------------------------------------------------------
-driver.get('https://www.minitex.co/?r=73238')
-time.sleep(10)
-driver.find_element(by=By.XPATH, value='//*[@id="main"]/section[1]/div[2]/div/div[1]/div/a[1]').click()
-time.sleep(10)
-driver.find_element(by=By.XPATH, value='//*[@id="username"]').send_keys(names)
-time.sleep(4)
-driver.find_element(by=By.XPATH, value='//*[@id="email"]').send_keys(main)
-time.sleep(5)
-driver.find_element(by=By.XPATH, value='//*[@id="password"]').send_keys(main2)
-time.sleep(6)
-driver.find_element(by=By.XPATH, value='//*[@id="confirm"]').send_keys(main2)
-time.sleep(6)
-driver.find_element(by=By.XPATH, value='//*[@id="register_form"]/div/div[7]/button').click()
-time.sleep(15)
-#driver.find_element(by=By.XPATH, value='//*[@id="main"]/section/div[2]/div/div/div[3]/div[1]/div/span').click()
-#time.sleep(10)
-#driver.find_element(by=By.XPATH, value='//*[@id="main"]/section/div[2]/div/div/div[3]/div[2]/a[3]').click()
-#time.sleep(10)
-#driver.find_element(by=By.XPATH, value='//*[@id="main"]/section/div[2]/div/div[1]/div[7]/div/div[4]/div/input').send_keys(3)
-#time.sleep(10)
-#driver.find_element(by=By.XPATH, value='//*[@id="main"]/section/div[2]/div/div[1]/div[7]/div/div[6]/button').click()
-#time.sleep(random.randint(25,60))
+from selenium.webdriver.common.by import By
+
+
+chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+
+chrome_options = Options()
+options = [
+    "--headless",
+    "--disable-gpu",
+    "--window-size=1920,1200",
+    "--ignore-certificate-errors",
+    "--disable-extensions",
+    "--no-sandbox",
+    "--disable-dev-shm-usage"
+]
+for option in options:
+    chrome_options.add_argument(option)
+
+driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+
+driver.get('http://nytimes.com')
+print(driver.title)
 
 
 
